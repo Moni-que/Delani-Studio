bba125f0ca4029228b728331b58d7bed6c2390ec
moni-que <monicahjustus@gmail.com>
2022-03-14 11:16:31 +0300

    add jQuery functions

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $("#design").click(function(){
+        $("#design").hide();
+        $("#ourdesign").show();
+    });
+    $("#ourdesign").click(function(){
+        $("#ourdesign").hide();
+        $("#design").show();
+    });
+    $("#development").click(function(){
+        $("#development").hide();
+        $("#ourdevelopment").show();
+    });
+    $("#ourdevelopment").click(function(){
+        $("#ourdevelopment").hide();
+        $("#development").show();
+    });
+    $("#planning").click(function(){
+        $("#planning").hide();
+        $("#ourplanning").show();
+    });
+    $("#ourplanning").click(function(){
+        $("#ourplanning").hide();
+        $("#planning").show();
+    });
+
+
+
+
+
+
+});
